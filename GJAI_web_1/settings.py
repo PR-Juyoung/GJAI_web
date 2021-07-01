dac4f8e5c935ef74705cfa93bd6f5072359f48fc
PR-Juyoung <9862wndud@gmail.com>
2021-07-01 10:28:32 +0900

    210701 hello_world commit

--- GJAI_web_1/settings.py
+++ GJAI_web_1/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent  # 최상위 프로젝트의 경로
 
 local_env = open(os.path.join(BASE_DIR, '.env'))
 
